2b7566b6e5c1989373c2fabd9223f92d64e35cb7
cerealenjoyer <sasha.antilyons@gmail.com>
2021-08-17 14:13:39 +0100

    formatting output

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 const contactForm = document.querySelector("form");
+const contactBoard = document.querySelector("#contactBoard");
 contactForm.addEventListener("submit", (event) => {
   event.preventDefault();
   validateAndSend(contactForm);
 });
 function validateAndSend(form) {
-  const formData = new FormData(contactForm);
+  const formData = new FormData(form);
   if (formData.get("email") == "" && formData.get("tel") == "") {
-    alert("Please give us a way to contact you.");
+    alert("Please enter either an email address or telephone number.");
     return false;
   } else if (
     formData.get("prefsemail") == null &&
@@ -15,6 +16,22 @@ function validateAndSend(form) {
     alert("Please select a contact preference");
     return false;
   } else {
-    form.submit();
+    displayDetails(Object.fromEntries(formData));
   }
 }
+
+function displayDetails(object) {
+  const contactCard = document.createElement("div");
+  contactCard.classList.add("box", "center");
+  const inputs = Object.entries(object);
+  inputs.forEach((a) => {
+    const entry = document.createElement("span");
+    if (a[0].slice(0, 5) === "prefs") {
+      entry.innerText = `Contact preference: ${capital(a[0].slice(5))}`;
+    } else entry.innerText = `${capital(a[0])} : ${a[1]}`;
+    contactCard.append(entry);
+    contactBoard.append(contactCard);
+  });
+}
+
+const capital = (str) => str.charAt(0).toUpperCase() + str.slice(1);
